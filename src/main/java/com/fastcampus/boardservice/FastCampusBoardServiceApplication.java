d955598142818cb2a0470626584320d4de5cee62
allegory <ikyoon0619@gmail.com>
2022-10-07 10:06:52 +0900

    #6 - use-case.svg를 revert
    잘못 리버트함.

---
+++ src/main/java/com/fastcampus/boardservice/FastCampusBoardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.boardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCampusBoardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastCampusBoardServiceApplication.class, args);
+    }
+
+}
